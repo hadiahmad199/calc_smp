508565988a25a5c67ef358b572284ae308fd002e
hadiahmad199 <168661027+hadiahmad199@users.noreply.github.com>
2026-01-30 14:07:22 +0300

    Update2 server.js

--- server.js
+++ server.js
@@ -66,6 +66,9 @@ app.get("/api/users/:id", async (req, res) => {
     };
 });
 
+app.get('/', (req, res) => res.send('Server is Up!'));
+
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is running on ${PORT} `);
