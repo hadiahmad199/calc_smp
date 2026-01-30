1558f0529062a1b91e4bccc2613a6d822dbd0cd8
hadiahmad199 <168661027+hadiahmad199@users.noreply.github.com>
2026-01-30 23:55:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.post("/api/users", async (req, res) => {
 // ====================
 // Start server
 // ====================
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port", PORT);
 });
